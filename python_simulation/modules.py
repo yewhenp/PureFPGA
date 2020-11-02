949893beba9661057a0bddaebfe3633c229071f4
Pavlo Hilei <p.hilei@ucu.edu.ua>
2020-11-02 19:12:24 +0200

    Add template of simulation

---
+++ python_simulation/modules.py
@@ -0,0 +1,35 @@
+class Memory:
+    def __init__(self):
+        pass
+
+    def write(self, address, data):
+        pass
+
+class ROM(Memory):
+    def __init__(self):
+        super().__init__()
+
+class RAM(Memory):
+    def __init__(self):
+        super().__init__()
+
+class Buffer(Memory):
+    def __init__(self):
+        super().__init__()
+
+class Core:
+    def __init__(self):
+        pass
+
+class MemoryManager:
+    def __init__(self):
+        pass
+
+class InstructionProc:
+    def __init__(self):
+        pass
+
+class Z80:
+    def __init__(self):
+        pass
+

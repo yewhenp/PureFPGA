2645c398a7bfac3e42430d9d392e2fa73513404b
Yevhen Pankevych <y.pankevych@ucu.edu.ua>
2020-11-02 21:23:11 +0200

    Implemented core and memory manager v1.2

--- python_simulation/core_modules.py
+++ python_simulation/core_modules.py
@@ -1,12 +1,14 @@
-from mem_modules import RAM, Buffer
+from mem_modules import RAM, Buffer, Register16, Register1
+from instructions import *
+
 
 class Core:
     def __init__(self):
         self.ram = RAM(2**16)
-        self.buffer_0 = Buffer()
-        self.buffer_1 = Buffer()
-        self.registers = [Register(), Register(), Register(), Register()]
-        self.buffer_register = 0
+        self.buffers = [Buffer(2**14), Buffer(2**14)]
+        self.registers = [Register16(), Register16(), Register16(), Register16()]
+        self.buffer_register = Register1()
+        self.buffer_use = Register1()
 
     def write_data(self, address, data):
         self.ram.write(address, data)
@@ -15,31 +17,35 @@ class Core:
         return self.ram.read(address)
 
     def execute(self, function, source_0, source_1):
-        function(source_0, source_1)
-
-    def _write_register(self, number, data):
-        self.registers[number].value = data
+        if function == load_to_mem:
+            function(self._read_source(source_0), self._read_source(source_1), self._get_ram())
+            if self._get_buffer_use():
+                function(self._read_source(source_0), self._read_source(source_1), self._get_buffer())
+        else:
+            function(self._read_source(source_0), self._read_source(source_1))
 
     def _get_register(self, number):
         return self.registers[number]
 
-    # def _read_register(self, number):
-    #     return self.registers[number].value
-    #
     def _read_source(self, source):
         if "reg" in source:
             return self._get_register(int(source[-1]))
-        return self.read_data(int(source))
-    #
-    # def _write_destination(self, destination, data):
-    #     if "reg" in destination:
-    #         return self._write_register(int(destination[-1]), data)
-    #     return self.write_data(int(destination), data)
+        return source
 
+    def _get_buffer_register(self):
+        return self.buffer_register
 
-class Register:
-    def __init__(self):
-        self.value = 0
+    def _get_buffer(self):
+        return self.buffers[self._get_buffer_register().read()]
+
+    def _get_buffer_use_register(self):
+        return self.buffer_use
+
+    def _get_buffer_use(self):
+        return self._get_buffer_use_register().read()
+
+    def _get_ram(self):
+        return self.ram
 
 
 class SM:
@@ -49,6 +55,11 @@ class SM:
         for _ in range(core_num):
             self.cores.append(Core())
 
+    def get_core(self, number):
+        if number >= self.core_num:
+            raise IndexError("No core with that index")
+        return self.cores[number]
+
 
 class MemoryManager:
     def __init__(self):
@@ -56,8 +67,8 @@ class MemoryManager:
 
     def write_data(self, address, data):
         core = address - (self.sm.core_num * (address // self.sm.core_num))
-        self.sm.cores[core].write_data(address // self.sm.core_num, data)
+        self.sm.get_core(core).write_data(address // self.sm.core_num, data)
 
     def read_data(self, address):
         core = address - (self.sm.core_num * (address // self.sm.core_num))
-        return self.sm.cores[core].read_data(address // self.sm.core_num)
+        return self.sm.get_core(core).read_data(address // self.sm.core_num)

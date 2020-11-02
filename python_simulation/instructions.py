cdaca78985fcdf13e0894da5449d5389dbe407f1
Tsapiv <volodja.tsv@gmail.com>
2020-11-02 23:10:43 +0200

    add flags

--- python_simulation/instructions.py
+++ python_simulation/instructions.py
@@ -6,35 +6,51 @@ CARRY_FLAG = 0
 
 def add_reg(reg1, reg2, flag):
     val = reg1.read() + reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if  val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def sub_reg(reg1, reg2, flag):
     val = reg1.read() - reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def and_reg(reg1, reg2, flag):
     val = reg1.read() & reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def or_reg(reg1, reg2, flag):
     val = reg1.read() | reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def xor_reg(reg1, reg2, flag):
     val = reg1.read() ^ reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def rshift_reg(reg1, reg2, flag):
     val = reg1.read() >> reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def lshift_reg(reg1, reg2, flag):
     val = reg1.read() << reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def mul_reg(reg1, reg2, flag):
     val = reg1.read() * reg2.read()
-    flag.write(reg1.write(val))
+    flag["zero_flag"] = 1 if not val else 0
+    flag["neg_flag"] = 1 if val < 0 else 0
+    flag["carry_flag"].write(reg1.write(val))
 
 def mov_reg(reg1, reg2, flag):
     reg1.write(reg2.read())
@@ -49,10 +65,10 @@ def load_to_mem(reg1: Register, reg2: Register, memory: Memory, flag):
     memory.write(reg1.read(), reg2.read())
 
 def ch_mod(reg1, reg2, flag):
-    flag.inc()
+    flag["mode_flag"].inc()
 
 def ch_buf(reg1, reg2, flag):
-    flag.inc()
+    flag["buffer_flag"].inc()
 
 #######################################################################
 
@@ -64,13 +80,16 @@ def sub_i(reg1, reg2, flag):
 
 def je(reg1, reg2, flag):
     """
-
-    :param reg1:
-    :param reg2:
-    :param flag:
+    :param reg1: instruction pointer
+    :param reg2: new position of instruction pointer
+    :param flag: Zero Flag
     :return:
     """
-    pass
+    if flag["zero_flag"]:
+        reg1.write(reg2.read())
 
-def mov_num_i(reg1, num):
+def mov_num_i(reg1, num, flag):
+    reg1.write(int(num))
+
+def nop(reg1, reg2, flag):
     pass

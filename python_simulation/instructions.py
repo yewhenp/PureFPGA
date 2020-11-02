8dad5c5ccbae601e6cb81c0385f089ae7cbde053
Pavlo Hilei <p.hilei@ucu.edu.ua>
2020-11-02 20:39:50 +0200

    implemented read program from file (InstructionProc)
    Also added venv to .gitignore
    Started working on instructions

---
+++ python_simulation/instructions.py
@@ -0,0 +1,48 @@
+CARRY_FLAG = 0
+
+
+def add_reg(reg1, reg2):
+    pass
+
+def sub_reg(reg1, reg2):
+    pass
+
+def and_reg(reg1, reg2):
+    pass
+
+def or_reg(reg1, reg2):
+    pass
+
+def xor_reg(reg1, reg2):
+    pass
+
+def rshift_reg(reg1, reg2):
+    pass
+
+def lshift_reg(reg1, reg2):
+    pass
+
+def mul_reg(reg1, reg2):
+    pass
+
+def mov_reg(reg1, reg2):
+    pass
+
+def mov_num(reg1, num):
+    pass
+
+
+#######################################################################
+
+def add_i(reg1, reg2):
+    pass
+
+def sub_i(reg1, reg2):
+    pass
+
+def je(reg1, reg2):
+    pass
+
+def mov_num_i(reg1, num):
+    pass
+

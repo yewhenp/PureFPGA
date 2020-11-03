261c286ee643280d6b25f6119daf762e49c1bbc0
Pavlo Hilei <p.hilei@ucu.edu.ua>
2020-11-03 13:52:44 +0200

    Add terminal clear on each instruction

--- python_simulation/instr_test.py
+++ python_simulation/instr_test.py
@@ -4,14 +4,16 @@ if __name__ == '__main__':
     videocard = VideoCard("tmp_program", number_of_cores=4)
     videocard.parse_VM("tmp_memory")
     videocard.get_inst_processor().change_ip(0)
-    print("VM: ", videocard.get_memory_manager().scan(4).to_string(16))
+    print(f"VM: {videocard.get_memory_manager().scan(4).to_string(16)}")
 
     for i in range(7):
-        input(">>> ")
         instruction = videocard.execute_next_instruction()
         print(f"Current instruction: {instruction[3]} {instruction[1]} {instruction[2]}\n")
-        print(videocard.to_string())
+        print(f"{videocard.to_string()}\n")
+        input(">>> ")
+        print('\033c')
+        print('\x1bc')
 
-    print(videocard.get_memory_manager().scan(4).to_string(16))
+    print(f"VM: {videocard.get_memory_manager().scan(4).to_string(16)}")
 
 

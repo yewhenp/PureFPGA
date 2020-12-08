bddbb0fa66ce0d8b40c586d6b1cbed4a0bcd42d3
Pavlo Hilei <p.hilei@ucu.edu.ua>
2020-12-08 21:34:02 +0200

    Small changes to instr_test. Remove __pycache__

--- python_simulation/test/instr_test.py
+++ python_simulation/test/instr_test.py
@@ -1,18 +1,23 @@
 from python_simulation.videocard_modules.videocard import VideoCard
 
 if __name__ == '__main__':
-    videocard = VideoCard("python_simulation/test/filter_res", number_of_cores=4)
+    program = "python_simulation/test/"
+    program += input("program: ")
+    videocard = VideoCard(program, number_of_cores=4)
     videocard.parse_VM("python_simulation/test/tmp_memory")
     videocard.get_inst_processor().change_ip(0)
     print(f"VM: {videocard.get_memory_manager().scan(4).to_string(16)}")
 
-    for i in range(100):
-        instruction = videocard.execute_next_instruction(True)
-        # print(f"Current instruction: {instruction}\n")
-        print(f"{videocard.to_string()}\n")
-        input(">>> ")
-        print('\033c')
-        print('\x1bc')
+    while True:
+        try:
+            instruction = videocard.execute_next_instruction(True)
+            # print(f"Current instruction: {instruction}\n")
+            print(f"{videocard.to_string()}\n")
+            input(">>> ")
+            print('\033c')
+            print('\x1bc')
+        except TypeError:
+            break
 
     print(f"VM: {videocard.get_memory_manager().scan(4).to_string(16)}")
 

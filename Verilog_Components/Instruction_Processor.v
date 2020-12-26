81ca0fde42fa601a2b42bad343467a0b4c325ded
Pavlo Hilei <p.hilei@ucu.edu.ua>
2020-12-26 19:20:07 +0200

    Refactor Instruction Processor; Add flag support

--- Verilog_Components/Instruction_Processor.v
+++ Verilog_Components/Instruction_Processor.v
@@ -4,7 +4,7 @@ module InstructionProcessor #(
     parameter
     WIDTH=16,
     REGS_CODING=3,
-    NOP=15'b1_0000_1011_00_00_00,
+    NOP=15'b1_0000_1011_00_00_00,   // addnv reg0, reg0
     CARRY=0,
     SIGN=1,
     OVERFLOW=2,
@@ -19,48 +19,18 @@ module InstructionProcessor #(
     output RAMWriteRead,
     output [WIDTH-2:0]instructionOut
 );
-    wire[3:0] ALUSel;
-    wire[15:0] firstOperand, secondOperand, ALURes;
-    wire cin, ALUCF, ALUSF, ALUOF, ALUZF, saveALURes;
+    wire[15:0] ALURes;
+    wire ALUCF, ALUSF, ALUOF, ALUZF;
 
     reg[14:0] coreInstr;
-    reg[3:0] ALUSelReg = 4'b1111;
-    reg[15:0] firstOperandReg=0, secondOperandReg=0, ALUResReg=0;
-    reg cinReg, saveALUResReg;
+    reg[3:0] ALUSel = 4'b1111;
+    reg[15:0] firstOperand=0, secondOperand=0, ALUResReg=0;
+    reg cinReg, saveALURes;
+
     assign instructionOut = coreInstr;
-    assign firstOperand = firstOperandReg;
-    assign secondOperand = secondOperandReg;
     assign ALURes = ALUResReg;
-    assign cin = cinReg;
-    assign saveALURes = saveALUResReg;
 
-    reg[15:0] reg0, reg1, reg2, reg3, reg4, reg5, sp, ip;
-    reg[3:0] flags; // CF, SF, OF, ZF
-    initial begin
-        reg0 <= 0;
-        reg1 <= 0;
-        reg2 <= 0;
-        reg3 <= 0;
-        reg4 <= 0;
-        reg5 <= 0;
-        sp <= 0;
-        ip <= 0;
-        // cin <= 0;
-        // saveALURes <= 0;
-    end
-
-    alu alu0 (
-        .A(firstOperand),
-        .B(secondOperand),
-        .ALUSel(ALUSel),
-        .CarryIn(cin),
-        .clk(clock),
-        .ALUOut(ALURes),
-        .CarryOut(ALUCF),
-        .SignOut(ALUSF),
-        .OverflowOut(ALUOF),
-        .ZeroOut(ALUZF)
-    );
+    reg[15:0] reg0, reg1, reg2, reg3, reg4, reg5, sp, ip, flags; // CF, SF, OF, ZF
 
     always @ (posedge clock) begin
         // if it's time to write into registers
@@ -72,9 +42,9 @@ module InstructionProcessor #(
                 3'b011: reg3 = regData;
                 3'b100: reg4 = regData;
                 3'b101: reg5 = regData;
-                3'b110: sp =   regData;
-                3'b111: ip =   regData;
-                default: ;
+                3'b110: sp   = regData;
+                3'b111: ip   = regData;
+                default:reg0 = reg0;
             endcase
         end else begin
             if (ROMData[0] == 1) begin
@@ -83,48 +53,48 @@ module InstructionProcessor #(
                 coreInstr <= NOP;
                 // alu command
                 if (ROMData[1] == 1) begin
-                    ALUSelReg <= ROMData[5:2];
+                    ALUSel <= ROMData[5:2];
                     case(ROMData[12:10])    // TODO: fix magic numbers
-                        3'b000: firstOperandReg = reg0;
-                        3'b001: firstOperandReg = reg1;
-                        3'b010: firstOperandReg = reg2;
-                        3'b011: firstOperandReg = reg3;
-                        3'b100: firstOperandReg = reg4;
-                        3'b101: firstOperandReg = reg5;
-                        3'b110: firstOperandReg = sp;
-                        3'b111: firstOperandReg = ip;
-                        default: firstOperandReg = 0;
+                        3'b000: firstOperand = reg0;
+                        3'b001: firstOperand = reg1;
+                        3'b010: firstOperand = reg2;
+                        3'b011: firstOperand = reg3;
+                        3'b100: firstOperand = reg4;
+                        3'b101: firstOperand = reg5;
+                        3'b110: firstOperand = sp;
+                        3'b111: firstOperand = ip;
+                        default: firstOperand = 0;
                     endcase
                     case(ROMData[15:13])    // TODO: fix magic numbers
-                        3'b000: secondOperandReg = reg0;
-                        3'b001: secondOperandReg = reg1;
-                        3'b010: secondOperandReg = reg2;
-                        3'b011: secondOperandReg = reg3;
-                        3'b100: secondOperandReg = reg4;
-                        3'b101: secondOperandReg = reg5;
-                        3'b110: secondOperandReg = sp;
-                        3'b111: secondOperandReg = ip;
-                        default: secondOperandReg = 0;
+                        3'b000: secondOperand = reg0;
+                        3'b001: secondOperand = reg1;
+                        3'b010: secondOperand = reg2;
+                        3'b011: secondOperand = reg3;
+                        3'b100: secondOperand = reg4;
+                        3'b101: secondOperand = reg5;
+                        3'b110: secondOperand = sp;
+                        3'b111: secondOperand = ip;
+                        default: secondOperand = 0;
                     endcase
                     // suffix
                     case(ROMData[9:6])    // TODO: fix magic numbers
-                        4'b0000: saveALUResReg = flags[ZERO] == 1;
-                        4'b0001: saveALUResReg = flags[ZERO] == 0;
-                        4'b0010: saveALUResReg = flags[ZERO] == 0 && flags[SIGN] == flags[OVERFLOW];
-                        4'b0011: saveALUResReg = flags[SIGN] != flags[OVERFLOW];
-                        4'b0100: saveALUResReg = flags[SIGN] == flags[OVERFLOW];
-                        4'b0101: saveALUResReg = flags[ZERO] == 1 || flags[SIGN] != OVERFLOW;
-                        4'b0110: saveALUResReg = flags[CARRY] == 1;
-                        4'b0111: saveALUResReg = flags[CARRY] == 0;
-                        4'b1000: saveALUResReg = flags[SIGN] == 1;
-                        4'b1001: saveALUResReg = flags[SIGN] == 0;
-                        4'b1010: saveALUResReg = 1;    // AL
-                        4'b1011: saveALUResReg = 0;    // NV
-                        4'b1100: saveALUResReg = flags[OVERFLOW] == 1;
-                        4'b1101: saveALUResReg = flags[OVERFLOW] == 0;
-                        4'b1110: saveALUResReg = flags[CARRY] == 1 && flags[ZERO] == 0;
-                        4'b1111: saveALUResReg = flags[CARRY] == 0 || flags[ZERO] == 0;
-                        default: saveALUResReg = 0;
+                        4'b0000: saveALURes = flags[ZERO] == 1;
+                        4'b0001: saveALURes = flags[ZERO] == 0;
+                        4'b0010: saveALURes = flags[ZERO] == 0 && flags[SIGN] == flags[OVERFLOW];
+                        4'b0011: saveALURes = flags[SIGN] != flags[OVERFLOW];
+                        4'b0100: saveALURes = flags[SIGN] == flags[OVERFLOW];
+                        4'b0101: saveALURes = flags[ZERO] == 1 || flags[SIGN] != OVERFLOW;
+                        4'b0110: saveALURes = flags[CARRY] == 1;
+                        4'b0111: saveALURes = flags[CARRY] == 0;
+                        4'b1000: saveALURes = flags[SIGN] == 1;
+                        4'b1001: saveALURes = flags[SIGN] == 0;
+                        4'b1010: saveALURes = 1;    // AL
+                        4'b1011: saveALURes = 0;    // NV
+                        4'b1100: saveALURes = flags[OVERFLOW] == 1;
+                        4'b1101: saveALURes = flags[OVERFLOW] == 0;
+                        4'b1110: saveALURes = flags[CARRY] == 1 && flags[ZERO] == 0;
+                        4'b1111: saveALURes = flags[CARRY] == 0 || flags[ZERO] == 0;
+                        default: saveALURes = 0;
                     endcase
                     // if suffix condition is true
                     if (saveALURes) begin
@@ -137,14 +107,35 @@ module InstructionProcessor #(
                             3'b101: reg5 = ALURes;
                             3'b110: sp = ALURes;
                             3'b111: ip = ALURes;
-                            default: reg0 = reg0;
+                            default: ;
                         endcase
+                        // cin if addc subc or mulc
+                        case(ROMData[5:2])
+                            4'b0001, 4'b0011, 4'b0101: cinReg <= 1;
+                            default: cinReg <= 0;
+                        endcase
+                        flags[CARRY]    <= ALUCF;
+                        flags[SIGN]     <= ALUSF;
+                        flags[OVERFLOW] <= ALUOF;
+                        flags[ZERO]     <= ALUZF;
                     end
                 end
-                ip = ip + 1;
             end
-
+            ip = ip + 1;
         end
     end
+
+    alu alu0 (
+        .A(firstOperand),
+        .B(secondOperand),
+        .ALUSel(ALUSel),
+        .CarryIn(cin),
+        .clk(clock),
+        .ALU_Out(ALURes),
+        .CarryOut(ALUCF),
+        .SignOut(ALUSF),
+        .OverflowOut(ALUOF),
+        .ZeroOut(ALUZF)
+    );
 
 endmodule

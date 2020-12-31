6c6a029428935ba791b9369bbe873d8ca62463f4
Pavlo Hilei <p.hilei@ucu.edu.ua>
2020-12-31 16:57:05 +0200

    Update memory manager and buffer manager

--- Verilog_Components/MemoryManager.v
+++ Verilog_Components/MemoryManager.v
@@ -1,38 +1,47 @@
 module memory_manager 
-#(parameter WIDTH=16,
-            ADDR_SPACE=22,
+#(parameter CORE_WIDTH=16,
+				DATA_WIDTH=8,
+            ADDR_SPACE=23,
             NUM_CORES=64,
             NUM_REGS=8,
             REGS_CODIND=3,
             CORE_CODIND=6)
    (	
-	input [WIDTH - 1:0]data, 
+	inout [DATA_WIDTH - 1:0]data, 
 	input [ADDR_SPACE - 1:0]address,
 	input wren_in,
 	input clk,
-	output [WIDTH - 1:0]core_data,
-	output [WIDTH - 1:0]core_address,
+	inout reg [CORE_WIDTH - 1:0]core_data,
+	output [CORE_WIDTH - 1:0]core_address,
 	output wren_out,
-    output reg [NUM_REGS - 1:0]reg_en,
+   output reg [NUM_REGS - 1:0]reg_en,
 	output reg [NUM_CORES - 1:0]core_en
 	);
   wire [NUM_CORES - 1:0]cores;
   wire [NUM_REGS - 1:0]regs;
-  assign core_data = data;
-  assign core_address = address[ADDR_SPACE - 1:CORE_CODIND];
+  assign core_address = address[ADDR_SPACE - 1:CORE_CODIND + 1];
   assign wren_out = wren_in;
   decoder64 d1(.data(address[CORE_CODIND - 1:0]), .eq63(cores));
   decoder8 d2(.data(address[REGS_CODIND - 1:0]), .eq7(regs));
   always @(posedge clk) begin
-    if(address > (2 << 22 - 8)) begin
-        core_en = 0;
-        reg_en = regs;
-      end 
-      else begin
-        reg_en = 0;
-        core_en = cores;
-      end
-    end
-  	
+		if(address > (2 << (ADDR_SPACE - 1) - NUM_REGS)) begin
+		  core_en = 0;
+		  reg_en = regs;
+		end 
+		else begin
+		  reg_en = 0;
+		  core_en = cores;
+		end
+	end
+	
+	always @(posedge clk) begin
+		if (address[0]) begin
+			core_data[DATA_WIDTH * 2 - 1: DATA_WIDTH] = data;
+		end
+		else begin
+			core_data[DATA_WIDTH - 1: 0] = data;
+		end
+	end
+
 
 endmodule

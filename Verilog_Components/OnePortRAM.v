ad5619c85d7bf2e0a401210cf3b06731006506f9
Pavlik1400 <pavlikhiley@gmail.com>
2021-01-16 21:06:06 +0200

    Fixing merge error

--- Verilog_Components/OnePortRAM.v
+++ Verilog_Components/OnePortRAM.v
@@ -4,19 +4,11 @@ module OnePortRAM
 	(
     output[DATA_WIDTH - 1:0] q,
     input [ADDR_SPACE - 1:0] address,
-<<<<<<< HEAD
     input [DATA_WIDTH - 1:0] data, 
     input wren,
     input clock);
 
     reg [DATA_WIDTH - 1:0] memory [0:(1 << ADDR_SPACE) - 1];
-=======
-    input [DATA_WIDTH-1:0] data, 
-    input wren,
-    input clock);
-		
-    reg [DATA_WIDTH-1:0] memory [0:(1 << ADDR_SPACE) - 1];
->>>>>>> e355c8b426a777e913b516ea784c0cec91f5b9c9
 
     always @(posedge clock) begin
         if (wren) begin

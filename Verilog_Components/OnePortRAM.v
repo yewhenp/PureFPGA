8da7d9d7a0de6d234665e29d26aa47a1a1d32560
Volodymyr <volodja.tsv@gmail.com>
2021-01-16 21:10:15 +0200

    I hate git (merge)

--- Verilog_Components/OnePortRAM.v
+++ Verilog_Components/OnePortRAM.v
@@ -2,30 +2,23 @@ module OnePortRAM
 #(parameter DATA_WIDTH=16,
             ADDR_SPACE=16)
 	(
-    output reg[DATA_WIDTH - 1:0] q,
+    output [DATA_WIDTH - 1:0] q,
     input [ADDR_SPACE - 1:0] address,
-<<<<<<< HEAD
     input [DATA_WIDTH - 1:0] data, 
     input wren,
     input clock);
 
     reg [DATA_WIDTH - 1:0] memory [0:(1 << ADDR_SPACE) - 1];
-=======
-    input [DATA_WIDTH-1:0] data, 
-    input wren,
-    input clock);
-		
-    reg [DATA_WIDTH-1:0] memory [0:(1 << ADDR_SPACE) - 1];
->>>>>>> e355c8b426a777e913b516ea784c0cec91f5b9c9
+
 
     always @(posedge clock) begin
         if (wren) begin
             memory[address] <= data;
         end
-		  q <= memory[address];
+//		  q <= memory[address];
 
     end
 
-    //assign q = memory[address];
+    assign q = memory[address];
 
 endmodule

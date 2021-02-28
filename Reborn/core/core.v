460290833a2ec78b6414a84c13688fdf8b1cb59d
Pavlik1400 <pavlikhiley@gmail.com>
2021-02-28 12:02:05 +0200

    Move all core parts to subdirectory

---
+++ Reborn/core/core.v
@@ -0,0 +1,22 @@
+module core #(
+ parameter
+ WIDTH=32,
+ REGS_CODING=8,
+ NOP=15'b1_0000_1011_00_00_00, // addnv reg0, reg0
+ CARRY=0,
+ SIGN=1,
+ OVERFLOW=2,
+ ZERO=3
+)(
+input 				 clk,
+input			     response,
+input	[WIDTH-1: 0] instruction,
+output 				 wren,
+output               request,
+input	[WIDTH-1: 0] readdata,
+output[WIDTH-1: 0]   address,
+output[WIDTH-1: 0]   writedata,
+output[WIDTH-1: 0]   instr_addr
+);
+
+endmodule
---
+++ Reborn/core/memory_ctrl.v
@@ -0,0 +1,9 @@
+module request_ctrl
+(
+input 				  		 clk,
+input 				  		 en,
+input                    reset,
+output                   ready
+);
+
+endmodule

7e905f02ba9e2557bc03d9e2156d5e78b2632db7
Pavlik1400 <pavlikhiley@gmail.com>
2021-05-28 17:56:49 +0300

    Change logic of result interupt from videocard

--- MCCP/videocard/memory_mapped_control.v
+++ MCCP/videocard/memory_mapped_control.v
@@ -16,7 +16,6 @@ output interrupt_internal
 reg [WIDTH-1: 0] data_start = 8'b0;
 reg [WIDTH-1: 0] data_finish = 8'b0;
 
-reg [WIDTH-1: 0] steps_out = 8'b0;
 reg [WIDTH-1: 0] steps_in = 8'b0;
 
 assign data_read = data_finish;
@@ -28,15 +27,9 @@ always @ (posedge clk) begin
 			if (address == 1'b1) begin
 				if (steps_out == 0) begin
 					data_finish = 1;
-					steps_out = 3;
 				end
 			end
 		end
-	end
-	if (steps_out > 0) begin
-		steps_out <= steps_out - 2'b01;
-	end else begin
-		data_finish = 0;
 	end
 end
 
@@ -44,6 +37,7 @@ always @(posedge clk_hps) begin
 	if (write) begin
 		if (address == 1'b0) begin
 			if (steps_in == 0) begin
+				data_finish = 0;
 				data_start = 1;
 				steps_in = 20;
 			end
